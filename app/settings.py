561474996e75c51eccd4713fb3cefaa74dc65c4b
Tourad Med Mahmoud Dahe <touraddah387@gmail.com>
2025-12-05 03:03:49 +0100

    IC

--- app/settings.py
+++ app/settings.py
@@ -68,6 +68,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -170,3 +171,6 @@ REST_FRAMEWORK = {
     ),
 
 }
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
